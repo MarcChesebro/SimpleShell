a9208b68b5e12096fbed233f32f3552f249e9c09
Kaylin <kzaroukian@gmail.com>
2019-01-22 23:10:27 -0500

    test

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,7 +11,7 @@ int main (int argc, char **argv){
 		char* cmdLine;
 		int status;
 		char* filePath;
-	
+
 		cmdLine = malloc(250);
 		filePath = malloc(250);
 
@@ -23,27 +23,56 @@ int main (int argc, char **argv){
 
 		char* cmd;
 		cmd = strtok(cmdLine, " ");
+		printf("command: %s\n", cmd);
 
-		if(strcmp(cmd, "quit")){
+		char** arg_vector = (char**)malloc(sizeof(char*));
+		memset(&arg_vector[0], 0, sizeof(arg_vector));
+
+		if(strcmp(cmd, "Quit") == 0){
+			printf("exiting...\n");
 			free(cmdLine);
 			free(filePath);
 			exit(0);
 		}else if(cmd != NULL) {
+			printf("entered\n");
+			int i = 0;
 			char* token = strtok(NULL, " ");
-			while(token != NULL){
-			
-				//add to vector
+			printf("yo\n");
 
-				token = strtok(NULL, " ");
+			printf("OG token: %s\n", token);
+			if(token) {
+				while(token){
+					printf("loop\n");
+
+					//add to vector
+					arg_vector[i] = token;
+					printf("token: %s\n",token );
+					printf("a_v: %s\n", arg_vector[i]);
+
+					token = strtok(NULL, " ");
+				}
 			}
-		}
 
-//		childPid = fork();
-//		if(childPid == 0){
-//			//use execvp() to run command
-//		}else{
-//			wait(&status);
-//			print status
-//
+			if (!token) {
+				printf("null token\n");
+				arg_vector[0] = "";
+			}
+
+		}
+		printf("vector: %s\n", arg_vector);
+
+		childPid = fork();
+		if(childPid == 0){
+			//use execvp() to run command
+			execvp(cmd,&arg_vector);
+			printf("executed\n");
+		}else{
+			printf("parent\n");
+			wait(&status);
+			printf("Status: %d\n",status);
+
 	}
+}
+
+return 0;
 }
